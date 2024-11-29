24618ffe9a2061779c3411e04f30dbb7c7c34897
Nenad Biletic <nenad.biletic@dci-student.org>
2024-11-29 14:35:12 +0100

    Added tests for service classes

--- src/main/java/com/dreamwork/model/user/Recruiter.java
+++ src/main/java/com/dreamwork/model/user/Recruiter.java
@@ -6,6 +6,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.OneToMany;
+import java.util.ArrayList;
 import java.util.List;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -19,7 +20,7 @@ public class Recruiter extends User {
 
   @OneToMany(mappedBy = "recruiter", cascade = CascadeType.ALL)
   @JsonIgnore // To prevent recursion when fetching JobAds that contain Recruiters
-  private List<JobAd> jobAds;
+  private List<JobAd> jobAds = new ArrayList<>();
 
   public Recruiter(String username, String password, String name, String lastname, String email) {
     super(username, password, name, lastname, email, Role.RECRUITER);
